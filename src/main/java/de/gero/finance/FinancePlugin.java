b6b8c10d42242458046576a2af463ef727dbab49
gerodeveloper <gero.fiverr@proton.me>
2022-12-27 12:28:31 +0100

    Added InventoryFramework as a possible GUI library

--- src/main/java/de/gero/finance/FinancePlugin.java
+++ src/main/java/de/gero/finance/FinancePlugin.java
@@ -1,11 +1,12 @@
 package de.gero.finance;
 
+import de.gero.finance.commands.FinanceTestCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FinancePlugin extends JavaPlugin {
     @Override
     public void onEnable() {
-
+        this.getCommand("financetest").setExecutor(new FinanceTestCommand());
     }
 
     @Override
