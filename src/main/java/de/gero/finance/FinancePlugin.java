4bf2b7b2b3920a82ceec62cf869a77f1884900e2
gerodeveloper <gero.fiverr@proton.me>
2022-12-27 11:37:54 +0100

    Gradle files and Main class

---
+++ src/main/java/de/gero/finance/FinancePlugin.java
@@ -0,0 +1,15 @@
+package de.gero.finance;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FinancePlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
